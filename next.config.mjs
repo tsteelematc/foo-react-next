637d6bddfad7a3b6d30f45c391fb20e743d0c824
tsteelematc <tsteele@madisoncollege.edu>
2024-09-16 11:09:43 -0500

    Messing with...
    . shadcn
    . deploying to GH Pages...

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    reactStrictMode: true,
+    basePath: "/foo-react-next"
+};
 
 export default nextConfig;
