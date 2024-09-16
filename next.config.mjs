f1aec5ca248986d122c3f87c25c4d1dadeb22746
tsteelematc <tsteele@madisoncollege.edu>
2024-09-16 13:12:39 -0500

    Blah...

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: "export",
-    reactStrictMode: true,
     basePath: "/foo-react-next",
     assetPrefix: "/foo-react-next/"
 };
